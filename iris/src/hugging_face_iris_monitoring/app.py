f748a12471131a7296c63fd7969bd3118539ad7c
Sebastiano Meneghin <sebastiano.meneghin00@gmail.com>
2023-11-20 03:08:32 +0100

    Categorized names of the pictures on the two projects

--- iris/src/hugging_face_iris_monitoring/app.py
+++ iris/src/hugging_face_iris_monitoring/app.py
@@ -11,8 +11,8 @@ dataset_api = project.get_dataset_api()
 
 dataset_api.download("Resources/images/latest_iris.png")
 dataset_api.download("Resources/images/actual_iris.png")
-dataset_api.download("Resources/images/df_recent.png")
-dataset_api.download("Resources/images/confusion_matrix.png")
+dataset_api.download("Resources/images/df_recent_iris.png")
+dataset_api.download("Resources/images/confusion_matrix_iris.png")
 
 with gr.Blocks() as demo:
     with gr.Row():
@@ -25,9 +25,9 @@ with gr.Blocks() as demo:
     with gr.Row():
       with gr.Column():
           gr.Label("Recent Prediction History")
-          input_img = gr.Image("df_recent.png", elem_id="recent-predictions")
+          input_img = gr.Image("df_recent_iris.png", elem_id="recent-predictions")
       with gr.Column():          
           gr.Label("Confusion Maxtrix with Historical Prediction Performance")
-          input_img = gr.Image("confusion_matrix.png", elem_id="confusion-matrix")        
+          input_img = gr.Image("confusion_matrix_iris.png", elem_id="confusion-matrix")        
 
 demo.launch()
--- iris/src/iris_batch_inference_pipeline.py
+++ iris/src/iris_batch_inference_pipeline.py
@@ -96,8 +96,8 @@ def g():
 
     # Take the recent history and create from it a graph, that than is saved on Hopsworks
     df_recent = history_df.tail(4)
-    dfi.export(df_recent, './df_recent.png', table_conversion = 'matplotlib')
-    dataset_api.upload("./df_recent.png", "Resources/images", overwrite=True)
+    dfi.export(df_recent, './df_recent_iris.png', table_conversion = 'matplotlib')
+    dataset_api.upload("./df_recent_iris.png", "Resources/images", overwrite=True)
     
     # Take the column of predictions and labels
     predictions = history_df[['prediction']]
@@ -114,8 +114,8 @@ def g():
     
         cm = sns.heatmap(df_cm, annot=True)
         fig = cm.get_figure()
-        fig.savefig("./confusion_matrix.png")
-        dataset_api.upload("./confusion_matrix.png", "Resources/images", overwrite=True)
+        fig.savefig("./confusion_matrix_iris.png")
+        dataset_api.upload("./confusion_matrix_iris.png", "Resources/images", overwrite=True)
     else:
         print("You need 3 different flower type predictions to create the confusion matrix.")
         print("Run the batch inference pipeline more times until you get 3 different iris flower type predictions") 
